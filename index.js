ae116867305abc295d05da9f46b1a6219ec8124a
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-05-01 01:21:23 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,6 +9,18 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+const uri = "mongodb+srv://mrrajon01:GEW4hD9rmf6zCnt@cluster-warehouse-manag.bfvdp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("db connected...");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 
 app.get('/', (req, res) => {
     res.send("Warehouse Management server is running...")
